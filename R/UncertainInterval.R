12da61b1b17b7c579871b4e963fb8a7a1d57e9e0
Hans Landsheer <j.a.landsheer@uu.nl>
2017-01-19 10:17:43 +0000

    version 0.3.0

---
+++ R/UncertainInterval.R
@@ -0,0 +1,12 @@
+#' @title Set of functions for the determination of an Uncertain Interval of test scores
+#'
+#' @description A collection of functions to determine a range of test scores that are inconclusive and do not allow a diagnosis (other than Uncertain) and to access its qualities.
+#'
+#' @seealso \code{\link{uncertain.interval}},  \code{\link{plotMD}},  \code{\link{get.intersection}},  \code{\link{quality.threshold}},  \code{\link{quality.threshold.uncertain}}
+#' @name UncertainInterval
+#' @references Landsheer, J. A. (2016). Interval of Uncertainty: An Alternative Approach for the Determination of Decision Thresholds, with an Illustrative Application for the Prediction of Prostate Cancer. PloS One, 11(11), e0166007.
+#' @details See Index
+NULL
+#> NULL
+## "_PACKAGE"
+##> [1] "_PACKAGE"
